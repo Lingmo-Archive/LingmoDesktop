54948445f3e11f7ec138e20cf71df60c1c39e5ac
Floating dream <2422586862@qq.com>
2023-04-09 20:13:59 +0800

    update 23.05.1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "desktop.h"
+#include "desktop/desktopsettings.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+
+    // Register QML Type.
+    const char *uri = "Cute.Desktop";
+
+    qmlRegisterType<DesktopSettings>(uri, 1, 0, "DesktopSettings");
+
+    Desktop desktop(argc,argv);
+    return desktop.run();
+}
