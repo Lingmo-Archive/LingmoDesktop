20238edcc970468d92093171857bd1877c4d6370
Floating dream <floatingdreamstudio@outlook.com>
2023-04-24 17:05:15 +0000

    update desktop/desktopsettings.cpp.
    Signed-off-by: Floating dream <floatingdreamstudio@outlook.com>

--- desktop/desktopsettings.cpp
+++ desktop/desktopsettings.cpp
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2021 CuteOS Team.
- *
- * Author:     revenmartin <revenmartin@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "desktopsettings.h"
 
 #include <QDBusServiceWatcher>
@@ -37,7 +18,7 @@ DesktopSettings::DesktopSettings(QObject *parent)
 
 QString DesktopSettings::wallpaper() const
 {
-    return m_wallpaper;
+    return m_wallpaper;//
 }
 
 bool DesktopSettings::backgroundVisible() const
